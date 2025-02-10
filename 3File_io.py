3252faa13263e491f27a08a6a430c6318a96da19
RationalStudent <shafqatameen316@gmail.com>
2025-02-10 21:55:00 +0530

    test a local photo

--- 3File_io.py
+++ 3File_io.py
@@ -4,6 +4,7 @@ load_img=o3d.data.JuneauImage()
 print(load_img.path)
 #read  sample data
 img=o3d.io.read_image(load_img.path)
+print(img)
 o3d.visualization.draw_geometries([img],"window name",width=1500,height=800)
 #o3d.io.write_image()
 o3d.io.write_image("datasets/img.png",img)

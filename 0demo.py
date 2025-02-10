087ef0020d6ce546cc06a13207cbfad6ae32357c
RationalStudent <shafqatameen316@gmail.com>
2025-02-11 01:36:39 +0530

    added the first vertex normal estimation

--- 0demo.py
+++ 0demo.py
@@ -3,9 +3,23 @@ import open3d as o3d
 # Load point cloud
 pcd = o3d.io.read_point_cloud("datasets/online_bunny.ply")
 print(pcd)
-# Create voxel grid
-voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=0.001)
-print(voxel_grid)
-# Visualize
-o3d.visualization.draw_geometries([pcd], "without downsampling applied")
-o3d.visualization.draw_geometries([voxel_grid],"voxel downsamoling applied")
+
+
+# Estimate normals
+pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
+# **1st Method: Orient normals to align with a given direction (e.g., [0, 0, 1])**
+pcd.orient_normals_to_align_with_direction(orientation_reference=[0, 0, 1])
+
+# Visualize the first orientation result
+print("Displaying normals aligned to direction [0, 0, 1]")
+o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+
+# **2nd Method: Orient normals to face the camera (e.g., at origin [0, 0, 0])**
+pcd.orient_normals_towards_camera_location(camera_location=[0, 0, 0])
+
+# Visualize the second orientation result
+print("Displaying normals facing camera at [0, 0, 0]")
+o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+
+

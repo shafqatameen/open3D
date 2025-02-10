6b072e6d03d03dc207687d01c16cd63a2d5f508a
RationalStudent <shafqatameen316@gmail.com>
2025-02-10 21:20:54 +0530

    learned the io file, load, read, write

---
+++ 3File_io.py
@@ -0,0 +1,9 @@
+import open3d as o3d
+#load image 
+load_img=o3d.data.JuneauImage()
+print(load_img.path)
+#read  sample data
+img=o3d.io.read_image(load_img.path)
+o3d.visualization.draw_geometries([img],"window name",width=1500,height=800)
+#o3d.io.write_image()
+o3d.io.write_image("datasets/img.png",img)

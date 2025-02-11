7b0cdde889c0dc297b86d77ae86a6c8ea87c87ec
RationalStudent <shafqatameen316@gmail.com>
2025-02-11 12:33:41 +0530

    i completed the visualization , voxel downsample and vertex normal estimation

--- 2visualization.py
+++ 2visualization.py
@@ -12,5 +12,6 @@ o3d.visualization.draw_geometries([pcd_xyz],"window name",1000,1000,
                                   up=[-0.0694, -0.9768, 0.2024])
 
 
-
+# - sign decrease the size of point and + sign will increase the size in the window of o3d
+#  
 
---
+++ 4vertex_estimate_normals.py
@@ -0,0 +1,20 @@
+import open3d as o3d
+# load read and visualize then voxel_down_sample and finally do the estimate the normals
+loaded_data=o3d.data.PLYPointCloud()
+pcd=o3d.io.read_point_cloud(loaded_data.path)
+#o3d.visualization.draw_geometries([pcd],window_name="vertex normal estimation",front=[0.4257, -0.2125, -0.8795])
+# Visualize the point cloud with custom camera parameters
+o3d.visualization.draw_geometries(
+    [pcd],
+    window_name="before Vertex Normal Estimation and voxel downsampling"
+    )
+
+#voxel down_sampling
+downpcd=pcd.voxel_down_sample(voxel_size=0.05)
+#visualize it
+o3d.visualization.draw_geometries([downpcd],"before Vertex Normal Estimation and  after voxel downsampling")
+
+#vertex estimat_normal 
+downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1,max_nn=30))
+#visualize it
+o3d.visualization.draw_geometries([downpcd],point_show_normal=True)

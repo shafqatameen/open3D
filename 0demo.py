3a8776d47121725dccbcd59e10d6e3c5def714b9
RationalStudent <shafqatameen316@gmail.com>
2025-02-14 10:41:53 +0530

    tranlate

--- 0demo.py
+++ 0demo.py
@@ -1,14 +1,22 @@
 import open3d as o3d
+import copy
 
-# Load the demo data
-demo_crop_data = o3d.data.DemoCropPointCloud()
+# Create a cube mesh
+cube = o3d.geometry.TriangleMesh.create_box(width=1.0, height=1.0, depth=1.0)
+cube.compute_vertex_normals()
+cube.paint_uniform_color([0.8, 0.2, 0.2])  # Red color for visualization
 
-# Read the point cloud
-pcd = o3d.io.read_point_cloud(demo_crop_data.point_cloud_path)
+# Create deep copies and translate them
+cube_tx = copy.deepcopy(cube)  # Copy for x-translation
+cube_ty = copy.deepcopy(cube)  # Copy for y-translation
 
-# Visualize the point cloud
-o3d.visualization.draw_geometries([pcd],
-                                  zoom=0.7,
-                                  front=[0.5439, -0.2333, -0.8060],
-                                  lookat=[2.4615, 2.1331, 1.338],
-                                  up=[-0.1781, -0.9708, 0.1608])
+cube_tx.translate((1.3, 0, 0))  # Move in x-direction
+cube_ty.translate((0, 1.3, 0))  # Move in y-direction
+
+# Print center positions
+print(f'Center of original cube: {cube.get_center()}')
+print(f'Center of cube_tx: {cube_tx.get_center()}')
+print(f'Center of cube_ty: {cube_ty.get_center()}')
+
+# Visualize original and translated cubes
+o3d.visualization.draw_geometries([cube, cube_tx, cube_ty])
